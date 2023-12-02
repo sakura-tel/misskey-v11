97c73536517ba837f0c199cdf7dbe7c6a16c3c02
mei23 <m@m544.net>
2023-12-02 18:02:47 +0900

    サイトバーにページとルームを追加 Resolve #2500

--- src/client/app/desktop/views/components/ui.sidebar.vue
+++ src/client/app/desktop/views/components/ui.sidebar.vue
@@ -20,6 +20,12 @@
 			<div class="game">
 				<a @click="game"><fa icon="gamepad"/><template v-if="hasGameInvitations"><fa icon="circle"/></template></a>
 			</div>
+			<div class="pages">
+				<router-link to="/i/pages"><fa :icon="faStickyNote"/></router-link>
+			</div>
+			<div class="room">
+				<router-link :to="`/@${ $store.state.i.username }/room`"><fa :icon="faDoorOpen"/></router-link>
+			</div>
 		</div>
 
 		<div class="nav bottom" v-if="$store.getters.isSignedIn">
@@ -62,7 +68,7 @@ import MkDriveWindow from './drive-window.vue';
 import MkMessagingWindow from './messaging-window.vue';
 import MkGameWindow from './game-window.vue';
 import contains from '../../../common/scripts/contains';
-import { faNewspaper, faHashtag } from '@fortawesome/free-solid-svg-icons';
+import { faNewspaper, faHashtag, faStickyNote, faDoorOpen } from '@fortawesome/free-solid-svg-icons';
 
 export default Vue.extend({
 	i18n: i18n('desktop/views/components/ui.sidebar.vue'),
@@ -72,7 +78,7 @@ export default Vue.extend({
 			connection: null,
 			showNotifications: false,
 			searching: false,
-			faNewspaper, faHashtag
+			faNewspaper, faHashtag, faStickyNote, faDoorOpen
 		};
 	},
 
