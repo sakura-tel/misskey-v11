add4fe2edce5187fdf9ae2299f99d1bf85fbc9b6
mei23 <m@m544.net>
2024-02-18 07:03:50 +0900

    ユーザーページのエラーででダイアログを表示するように Fix #2518

--- src/client/app/mobile/views/pages/user/index.vue
+++ src/client/app/mobile/views/pages/user/index.vue
@@ -148,6 +148,10 @@ export default Vue.extend({
 
 				Progress.done();
 				document.title = `${Vue.filter('userName')(this.user)} | ${this.$root.instanceName}`;
+			}).catch((e: any) => {
+				this.$root.dialog({ type: 'error', text: e.message || 'Error' });
+				this.fetching = false;
+				Progress.done();
 			});
 		},
 
