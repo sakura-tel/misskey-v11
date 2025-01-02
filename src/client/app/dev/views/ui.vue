b37a29ed93d712560a6a523436913513ef896645
fs5m8 <fsco3092@protonmail.com>
2025-01-03 06:37:08 +0900

    bootstrap 4.6.2 -> 5.3.3

--- src/client/app/dev/views/ui.vue
+++ src/client/app/dev/views/ui.vue
@@ -1,6 +1,6 @@
 <template>
 <div>
-	<b-navbar toggleable="md" type="dark" variant="info">
+	<b-navbar toggleable="md">
 		<b-navbar-brand>Developers</b-navbar-brand>
 		<b-navbar-nav>
 			<b-nav-item to="/">Home</b-nav-item>
@@ -14,6 +14,9 @@
 </template>
 
 <style lang="stylus" scoped>
+.navbar
+	--bs-navbar-padding-x 1rem
+	--bs-navbar-padding-y 0.5rem
 main
 	padding 32px
 	max-width 700px
